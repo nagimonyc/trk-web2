a1ac9113fec80048f80a332d02354f68d8db3f46
VGmt <Victor.guillemot@gmail.com>
2024-04-16 17:57:25 -0400

    first push

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import backgroundImage from './images/IMG_8105.jpeg';
+import backgroundImage2 from './images/IMG_8105.jpeg';
+import backgroundImage1 from './images/Desktop-Stripe-integration-2.png';
 
 
 const Home = () => {
+    const [backgroundImage, setBackgroundImage] = React.useState(window.innerWidth > 768 ? backgroundImage1 : backgroundImage2);
+
+    React.useEffect(() => {
+        const handleResize = () => {
+            setBackgroundImage(window.innerWidth > 768 ? backgroundImage1 : backgroundImage2);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <div style={{ backgroundImage: `url(${backgroundImage})`, height: '100vh', width: '100vw', backgroundRepeat: 'no-repeat', backgroundSize: 'contain', backgroundPosition: 'center' }}>
+        <div style={{ backgroundImage: `url(${backgroundImage})`, height: '100vh', flex: 1, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' }}>
         </div>
     );
 }
