4c5da4d1d5f4b4be3676a3ae4e697fa725275170
VGmt <Victor.guillemot@gmail.com>
2024-08-08 13:57:29 -0400

    event updates

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -14,6 +14,9 @@ const Partners = () => {
         <div className="partners-container" >
             <h1 className="partners-title">Events</h1>
             <div className="partners-list">
+                <PartnersBox time="6:00 PM" place="partner Place 3" url="" image="Nagimondays-4.avif" />
+                <PartnersBox time="6:00 PM" place="partner Place 3" url="" image="NagiBingo.avif" />
+                <PartnersBox time="6:00 PM" place="partner Place 3" url="" image="Nagimondays-3.avif" />
                 <PartnersBox time="6:00 PM" place="partner Place 3" url="" image="Nagimondays-2.avif" />
                 <PartnersBox time="BKB" place="partner Place 1" url="https://www.instagram.com/p/C95nQolStIX/" image="Meetup-BKB2.avif" />
                 <PartnersBox time="6:00 PM" place="partner Place 3" url="" image="Nagimondays-1.avif" />
