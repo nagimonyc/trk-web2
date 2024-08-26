4b4a5addae7cfb581872e5850b5a88c04e57f2b3
VGmt <Victor.guillemot@gmail.com>
2024-08-26 18:34:03 +0200

    test

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -91,7 +91,7 @@ const Subscription = () => {
                             <li>Freeze possible</li>
                         </ul>
                     </div>
-                    <p style={{ marginBottom: 15, fontWeight: '700', color: '#2eba88', fontSize: 12 }}>* based on the price of 4 memberships in NY</p>
+                    <p style={{ marginBottom: 15, fontWeight: '700', color: '#2eba88', fontSize: 12 }}>* based on the price of 3 memberships in NY</p>
                 </div>
             </div>
         </div>
