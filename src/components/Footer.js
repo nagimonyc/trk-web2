cd38ea194f069dba0ffc4d6d3911fe1ed644b4e4
VGmt <Victor.guillemot@gmail.com>
2024-09-12 13:02:21 -0400

    instalogo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,18 @@
 import React from "react";
 import './Footer.css';
 import nagimoTeam from '../images/IMG_6777-1.png';
+import instagramLogo from '../images/Instagram_logo.png';
 
 const Footer = ({ page }) => {
     return (
         <div className="footer-container">
             <img src={nagimoTeam} className="footer-img" alt="Nagimo Team" />
             <div className="footer-messaging">
-                <p>A community built on the mat • <a href="mailto:nagimo.nyc@nagimo.org" style={{ textDecoration: 'underline', cursor: 'pointer', color: 'black' }}>Contact us</a></p>
+                <p>A community built on the mat • Join us
+                    <a href="https://www.instagram.com/nagimosends/">
+                        <img src={instagramLogo} alt="Instagram" style={{ height: 25 }} />
+                    </a>
+                </p>
             </div>
         </div>
     );
